845df049c847339161d799ab59e586183c76afab
mariapbn1 <mariaplml190@gmail.com>
2021-07-24 16:45:15 -0500

    corrección de errores en base de datos

--- testServiBack/api/views.py
+++ testServiBack/api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import TagsSerializer, TagsGetSerializes
 
 
 # Create your views here.
